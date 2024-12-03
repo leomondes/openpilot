abe7fe96f6cd55518e027da408fcf70052099810
leomonde <gianlucafedele@gmail.com>
2024-12-03 23:20:14 +0100

    Update safety.h

--- panda/board/safety.h
+++ panda/board/safety.h
@@ -318,7 +318,7 @@ const safety_hook_config safety_hook_registry[] = {
   {SAFETY_HYUNDAI_CANFD, &hyundai_canfd_hooks},
 #endif
 #ifdef ALLOW_DEBUG
-  {SAFETY_JEEP, &JEEP_hooks},
+  {SAFETY_JEEP, &jeep_hooks},
   {SAFETY_TESLA, &tesla_hooks},
   {SAFETY_SUBARU_PREGLOBAL, &subaru_preglobal_hooks},
   {SAFETY_VOLKSWAGEN_PQ, &volkswagen_pq_hooks},
