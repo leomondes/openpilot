cc8291ded5ddb065982e9a4811c1904452ed6b7e
leomonde <gianlucafedele@gmail.com>
2024-12-03 19:56:23 +0100

    Update test_jeep.py

--- panda/tests/safety/test_jeep.py
+++ panda/tests/safety/test_jeep.py
@@ -52,9 +52,9 @@ class TestJeepSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafet
     values = {"ACCEL_PEDAL_FOOT": 1 if gas_pressed > 0 else 0}
     return self.packer.make_can_msg_panda("ENGINE_2", 0, values)
 
-  #def _torque_driver_msg(self, driver_torque):
-  #  values = {"DRIVER_TORQUE": driver_torque}
-  #  return self.packer.make_can_msg_panda("EPS_2", 0, values)
+  def _torque_driver_msg(self, driver_torque):
+    values = {"DRIVER_TORQUE": driver_torque}
+    return self.packer.make_can_msg_panda("EPS_2", 0, values)
 
   def _torque_meas_msg(self, torque):
     values = {"EPS_TORQUE": torque}
