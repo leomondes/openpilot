5e994748bdd1494d8ae4bf174f9d8dcf7e15977d
leomonde <gianlucafedele@gmail.com>
2024-12-04 19:33:35 +0100

    Update values.py

--- opendbc_repo/opendbc/car/jeep/values.py
+++ opendbc_repo/opendbc/car/jeep/values.py
@@ -28,7 +28,8 @@ class CANBUS:
 
 @dataclass
 class JeepPlatformConfig(PlatformConfig):
-  dbc_dict: DbcDict = field(default_factory=lambda: dbc_dict('renegade', None))
+  #dbc_dict: DbcDict = field(default_factory=lambda: dbc_dict('renegade', None))
+  dbc_dict: DbcDict = field(default_factory=lambda: {Bus.pt: 'renegade'})
 
 
 @dataclass(frozen=True, kw_only=True)
