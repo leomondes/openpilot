6c427fee314e4bbf141466de06941c4668fb95b4
leomonde <gianlucafedele@gmail.com>
2024-12-05 12:20:21 +0100

    back

--- panda/tests/safety/test_jeep.py
+++ panda/tests/safety/test_jeep.py
@@ -5,11 +5,6 @@ from panda.tests.libpanda import libpanda_py
 import panda.tests.safety.common as common
 from panda.tests.safety.common import CANPackerPanda
 
-def checksum(msg):
-  addr, dat, bus = msg
-  if addr == 0xEE:
-    ret = dat[7]
-  return addr, ret, bus
 
 class TestJeepSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafetyTest):
   TX_MSGS = [[0x1F6, 0], [0x547, 0], [0x5A2, 0]]
@@ -43,7 +38,7 @@ class TestJeepSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafet
 
   def _speed_msg(self, speed):
     values = {"WHEEL_SPEED_%s" % s: speed for s in ["FL", "FR", "RL", "RR"]}
-    return self.packer.make_can_msg_panda("ABS_1", 0, values, fix_checksum=checksum)
+    return self.packer.make_can_msg_panda("ABS_1", 0, values)
 
   def _speed_msg_2(self, speed):
     values = {"VEHICLE_SPEED": speed}
@@ -69,13 +64,13 @@ class TestJeepSafety(common.PandaCarSafetyTest, common.DriverTorqueSteeringSafet
     values = {"LKA_TORQUE": apply_torque, "LKA_ENABLED": 1 if steer_req else 0}
     return self.packer.make_can_msg_panda("LKA_COMMAND", 0, values)
 
-  #def test_rx_hook(self):
-  #  for count in range(20):
-  #    self.assertTrue(self._rx(self._speed_msg(0)), f"{count=}")
-  #    self.assertTrue(self._rx(self._user_brake_msg(True)), f"{count=}")
-  #    self.assertTrue(self._rx(self._user_gas_msg(True)), f"{count=}")
-  #    self.assertTrue(self._rx(self._torque_meas_msg(0)), f"{count=}")
-  #    self.assertTrue(self._rx(self._pcm_status_msg(False)), f"{count=}")
+  def test_rx_hook(self):
+    for count in range(20):
+      self.assertTrue(self._rx(self._speed_msg(0)), f"{count=}")
+      self.assertTrue(self._rx(self._user_brake_msg(True)), f"{count=}")
+      self.assertTrue(self._rx(self._user_gas_msg(True)), f"{count=}")
+      self.assertTrue(self._rx(self._torque_meas_msg(0)), f"{count=}")
+      self.assertTrue(self._rx(self._pcm_status_msg(False)), f"{count=}")
 
 if __name__ == "__main__":
   unittest.main()
