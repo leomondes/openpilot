77144095964f62219c53eae1cc4892263c8e3407
leomonde <gianlucafedele@gmail.com>
2024-12-05 12:47:24 +0100

    try init pressed

--- panda/board/safety/safety_jeep.h
+++ panda/board/safety/safety_jeep.h
@@ -94,6 +94,9 @@ static uint32_t jeep_compute_crc(const CANPacket_t *to_push) {
 static safety_config jeep_init(uint16_t param) {
   UNUSED(param);
 
+  brake_pressed = false;
+  gas_pressed = false;
+
   gen_crc_lookup_table_8(0x2F, jeep_crc8_lut_j1850);
   return BUILD_SAFETY_CFG(jeep_rx_checks, JEEP_TX_MSGS);
 }
