6a43c9d8802c529d6a49f320ceb57ca2c3392961
leomonde <gianlucafedele@gmail.com>
2024-12-11 00:12:14 +0100

    Revert "fix"
    This reverts commit 99fb893900984d501fa4344282d775dc7f5a41a5.

--- panda/board/safety/safety_jeep.h
+++ panda/board/safety/safety_jeep.h
@@ -151,10 +151,10 @@ static void jeep_rx_hook(const CANPacket_t *to_push) {
     if ((GET_BUS(to_push) == 1U) && (addr == JEEP_ACC_2)) {
       // When using stock ACC, enter controls on rising edge of stock ACC engage, exit on disengage
       unsigned int acc_status = (GET_BYTE(to_push, 4) & 0x0FU);
-      cruise_engaged = (acc_status == 6) || (acc_status == 7) || (acc_status == 8);
+      bool cruise_engaged = (acc_status == 6) || (acc_status == 7) || (acc_status == 8);
     } else if ((GET_BUS(to_push) == 0U) && (addr == JEEP_ACC_5)) {
       // When using stock ACC, enter controls on rising edge of stock ACC engage, exit on disengage
-      cruise_engaged = GET_BIT(to_push, 2U);
+      bool cruise_engaged = GET_BIT(to_push, 2U);
     }
     pcm_cruise_check(cruise_engaged);
   }
