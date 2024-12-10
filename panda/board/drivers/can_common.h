9791e39d33c0ee00097744defb6d433706361bb0
leomonde <gianlucafedele@gmail.com>
2024-12-11 00:29:59 +0100

    try fix ignition can

--- panda/board/drivers/can_common.h
+++ panda/board/drivers/can_common.h
@@ -188,7 +188,7 @@ void ignition_can_hook(CANPacket_t *to_push) {
 
     // Renegade exception
     if ((addr == 0xFA) && (len == 8)) {
-      ignition_can = (GET_BIT(to_push, 5)) != 0U;
+      ignition_can = (GET_BIT(to_push, 5)) != 1U;
       ignition_can_cnt = 0U;
     }
 
