97f3e18ed61766a3c0bbf50a7c4709f855a68873
leomonde <gianlucafedele@gmail.com>
2024-12-12 10:32:53 +0100

    Revert "try fix"
    This reverts commit 86b9b4ffe4b5bbd9a18ebf60c21b1593f39bc7e1.

--- panda/board/safety/safety_jeep.h
+++ panda/board/safety/safety_jeep.h
@@ -152,11 +152,10 @@ static void jeep_rx_hook(const CANPacket_t *to_push) {
   // On ACC_2 we have the cruise engaged variable
   // On ACC_5 we have ACC braking bit, while ACC is braking ACC_2 goes temporarily to zero
   if ((addr == JEEP_ACC_2) || (addr == JEEP_ACC_5)) {
-    int acc_status = 0;
     if ((GET_BUS(to_push) == 1U) && (addr == JEEP_ACC_2)) {
-      acc_status = (GET_BYTE(to_push, 4) & 0x0FU);
+      int acc_status = (GET_BYTE(to_push, 4) & 0x0FU);
     } else if ((GET_BUS(to_push) == 0U) && (addr == JEEP_ACC_5)) {  
-      acc_status = (GET_BIT(to_push, 2U));  
+      int acc_status = (GET_BIT(to_push, 2U));  
     }
     bool cruise_engaged = (acc_status == 6) || (acc_status == 7) || (acc_status == 8) || (acc_status == 1);
     pcm_cruise_check(cruise_engaged);
