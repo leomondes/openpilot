3ad6549fa8a629c085493c394a9fefc6ada0a923
leomonde <gianlucafedele@gmail.com>
2024-12-12 11:27:31 +0100

    try without zero

--- panda/board/safety/safety_jeep.h
+++ panda/board/safety/safety_jeep.h
@@ -146,11 +146,10 @@ static void jeep_rx_hook(const CANPacket_t *to_push) {
   }
   
   if (((GET_BUS(to_push) == 1U) && (addr == JEEP_ACC_2)) || ((GET_BUS(to_push) == 0U) && (addr == JEEP_ACC_5))) {
-    int acc_status = 0;
     if ((GET_BUS(to_push) == 1U) && (addr == JEEP_ACC_2)) {
-      acc_status = (GET_BYTE(to_push, 4) & 0x0FU);
+      int acc_status = (GET_BYTE(to_push, 4) & 0x0FU);
     } else if ((GET_BUS(to_push) == 0U) && (addr == JEEP_ACC_5)) {  
-      acc_status = (GET_BIT(to_push, 2U));  
+      int acc_status = (GET_BIT(to_push, 2U));  
     }
     bool cruise_engaged = (acc_status == 6) || (acc_status == 7) || (acc_status == 8) || (acc_status == 1);
     pcm_cruise_check(cruise_engaged);
