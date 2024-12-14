6c95091682630a9b9c1bc8d3628de45f1b05bc42
leomonde <gianlucafedele@gmail.com>
2024-12-14 02:09:50 +0100

    Update test_car_model.py

--- tools/car_porting/test_car_model.py
+++ tools/car_porting/test_car_model.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
   sr = SegmentRange(args.route_or_segment_name)
 
   print(sr.seg_idxs)
-  del sr.seg_idxs[-1]
+  sr.seg_idxs.pop()
   print(sr.seg_idxs)
 
   test_routes = [CarTestRoute(sr.route_name, args.car, segment=seg_idx) for seg_idx in sr.seg_idxs]
