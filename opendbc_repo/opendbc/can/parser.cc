328363c9fc2fb969937f6136b48c569939dd8acf
leomonde <gianlucafedele@gmail.com>
2024-12-14 02:38:10 +0100

    Update parser.cc

--- opendbc_repo/opendbc/can/parser.cc
+++ opendbc_repo/opendbc/can/parser.cc
@@ -74,9 +74,9 @@ bool MessageState::parse(uint64_t nanos, const std::vector<uint8_t> &dat) {
 
 
 bool MessageState::update_counter_generic(int64_t v, int cnt_size) {
-  if (((counter + 1) & ((1 << cnt_size) -1)) != v) {
+  if (((counter + 1) & ((1 << cnt_size))) != v) {
     counter_fail = std::min(counter_fail + 1, MAX_BAD_COUNTER);
-    if (counter_fail > MAX_BAD_COUNTER) {
+    if (counter_fail > 1) {
       INFO("0x%X COUNTER FAIL #%d -- %d -> %d\n", address, counter_fail, counter, (int)v);
     }
   } else if (counter_fail > 0) {
