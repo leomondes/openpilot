d5ff18bb8dbb5a55ee10b665324761beacb334d9
leomonde <gianlucafedele@gmail.com>
2024-12-14 01:35:25 +0100

    test

--- opendbc_repo/opendbc/can/tests/test_packer_parser.py
+++ opendbc_repo/opendbc/can/tests/test_packer_parser.py
@@ -5,7 +5,7 @@ from opendbc.can.parser import CANParser
 from opendbc.can.packer import CANPacker
 from opendbc.can.tests import TEST_DBC
 
-MAX_BAD_COUNTER = 50
+MAX_BAD_COUNTER = 500
 
 
 class TestCanParserPacker:
