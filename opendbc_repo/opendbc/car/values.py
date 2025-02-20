477e246327a1d2effc51fe29028be1804b0317bb
leomonde <gianlucafedele@gmail.com>
2025-02-20 19:19:41 -0300

    Add VOLVO in values (brands)

--- opendbc_repo/opendbc/car/values.py
+++ opendbc_repo/opendbc/car/values.py
@@ -13,8 +13,9 @@ from opendbc.car.subaru.values import CAR as SUBARU
 from opendbc.car.tesla.values import CAR as TESLA
 from opendbc.car.toyota.values import CAR as TOYOTA
 from opendbc.car.volkswagen.values import CAR as VOLKSWAGEN
+from opendbc.car.volvo.values import CAR as VOLVO
 
-Platform = BODY | CHRYSLER | JEEP | FORD | GM | HONDA | HYUNDAI | MAZDA | MOCK | NISSAN | SUBARU | TESLA | TOYOTA | VOLKSWAGEN
+Platform = BODY | CHRYSLER | JEEP | FORD | GM | HONDA | HYUNDAI | MAZDA | MOCK | NISSAN | SUBARU | TESLA | TOYOTA | VOLKSWAGEN | VOLVO
 BRANDS = get_args(Platform)
 
 PLATFORMS: dict[str, Platform] = {str(platform): platform for brand in BRANDS for platform in brand}
