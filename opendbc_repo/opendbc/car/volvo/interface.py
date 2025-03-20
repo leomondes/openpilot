85b35d6a6a63366b3d9dedeea42b780d9095cc04
leomonde <gianlucafedele@gmail.com>
2025-03-20 11:57:40 -0300

    9th try to fix error

--- opendbc_repo/opendbc/car/volvo/interface.py
+++ opendbc_repo/opendbc/car/volvo/interface.py
@@ -11,7 +11,7 @@ class CarInterface(CarInterfaceBase):
   def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
     ret.carName = "volvo"
     ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.volvo)]
-    ret.safetyConfigs[0].safetyParam |= Panda.SAFETY_VOLVO
+    ret.safetyConfigs[-1].safetyParam |= Panda.SAFETY_VOLVO
 
     # ret.dashcamOnly = True
     ret.radarUnavailable = True
