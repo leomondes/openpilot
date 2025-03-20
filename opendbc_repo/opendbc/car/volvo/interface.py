d28fd3061d1d36e29a5417f7653ca731603f81a1
leomonde <gianlucafedele@gmail.com>
2025-03-19 21:42:41 -0300

    1st try to avoid error in car porting script

--- opendbc_repo/opendbc/car/volvo/interface.py
+++ opendbc_repo/opendbc/car/volvo/interface.py
@@ -1,5 +1,4 @@
-from cereal import car
-from opendbc.car import get_safety_config
+from opendbc.car import get_safety_config, structs
 from opendbc.car.interfaces import CarInterfaceBase
 from opendbc.car.volvo.values import CAR
 
@@ -8,9 +7,9 @@ from opendbc.car.volvo.values import CAR
 
 class CarInterface(CarInterfaceBase):
   @staticmethod
-  def _get_params(ret, candidate: CAR, fingerprint, car_fw, experimental_long, docs):
+  def _get_params(ret: structs.CarParams, candidate, fingerprint, car_fw, experimental_long, docs) -> structs.CarParams:
     ret.carName = "volvo"
-    ret.safetyConfigs = [get_safety_config(car.CarParams.SafetyModel.volvo)]
+    ret.safetyConfigs = [get_safety_config(structs.CarParams.SafetyModel.volvo)]
     # ret.dashcamOnly = True
     ret.radarUnavailable = True
 
