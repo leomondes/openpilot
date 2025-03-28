f601d039cdd462dc5953d93323d34edee1865b9d
leomonde <129780808+leomonde@users.noreply.github.com>
2025-03-28 00:08:16 -0300

    Update values.py

--- opendbc_repo/opendbc/car/volvo/values.py
+++ opendbc_repo/opendbc/car/volvo/values.py
@@ -98,7 +98,7 @@ class VolvoCarDocs(CarDocs):
 
 @dataclass(frozen=True)
 class VolvoCarSpecs(CarSpecs):
-  steerRatio: float = 15.0
+  steerRatio: float = 14.0
   centerToFrontRatio: float = 0.44
   minSteerSpeed: float = 1.0
 
