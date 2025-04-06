a69daf4aaf187b30af842151d338c364b149498c
leomonde <129780808+leomonde@users.noreply.github.com>
2025-04-05 21:55:05 -0300

    Update values.py

--- opendbc_repo/opendbc/car/volvo/values.py
+++ opendbc_repo/opendbc/car/volvo/values.py
@@ -73,7 +73,7 @@ class CarControllerParams:
   # When close to desired steering angle, don't change steer direction inside deadzone.
   # Since we need to release control of the steering wheel for a brief moment, steering wheel will
   # unwind by itself.
-  DEADZONE = 0.5
+  DEADZONE = 0.05
 
   def __init__(self, CP):
     can_define = CANDefine(DBC[CP.carFingerprint][Bus.pt])
