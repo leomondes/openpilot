252ec0cb82fdfbbc7059d6e2ce4a6df60002a5af
leomonde <gianlucafedele@gmail.com>
2025-06-28 15:56:00 -0300

    Try to use same AGNOS of 0.9.9 - version 12.4

--- launch_env.sh
+++ launch_env.sh
@@ -7,9 +7,8 @@ export OPENBLAS_NUM_THREADS=1
 export VECLIB_MAXIMUM_THREADS=1
 
 if [ -z "$AGNOS_VERSION" ]; then
-  export AGNOS_VERSION="11.13"
+  export AGNOS_VERSION="12.4"
 fi
 
 export STAGING_ROOT="/data/safe_staging"
-
 export FINGERPRINT="VOLVO_V60"
